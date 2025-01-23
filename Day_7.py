c847745fe1b9e43f22459fa8d4e7d809e49e4800
Rishitha-Reddy-143 <2311cs020718@mallareddyuniversity.ac.in>
2025-01-23 20:01:31 +0530

    Add files via upload

---
+++ Day_7.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+df=pd.read_csv(r'C:\dataset\Day_7_sales_data.csv')
+df
+
+
+# In[3]:
+
+
+df[df['Sales']>1000]
+
+
+# In[6]:
+
+
+df['Profit_Per_Unit']=df['Profit']/df['Quantity']
+df
+
+
+# In[11]:
+
+
+df['High_Sales']=((df['Sales']>1000)=='Yes')
+df
+
+
+# In[ ]:
+
+
+
+
